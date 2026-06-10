ca3d70a13f9365c6e61442827e5e0185dde31fe3
KaiZenRX7 <sebastianfigs@hotmail.com>
2026-06-10 18:39:26 -0400

    Feat: Inicialización de EVetCore y configuración base para la rama Pupis & Michis

--- src/Veterinaria.App/App.xaml.cs
+++ src/Veterinaria.App/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new MainPage()) { Title = "Veterinaria.App" };
+		return new Window(new MainPage()) { Title = "EVetCore - Pupis Michi" };
 	}
 }
